c9d80a1cc269c5f9a60b6b51e72c8c9e6d3c6122
UH3135 <rabbitdy0101@kookmin.ac.kr>
2020-11-20 13:23:28 +0900

    make classTile

--- GUI/UI.py
+++ GUI/UI.py
@@ -19,3 +19,26 @@ functionList = ["Gyeol", "Hap", "del", "clear"]
 roundList = ["Round" + " " + str(i) for i in range(1, 10)]
 
 
+class Tile:
+    def __init__(self):
+
+        self.tileDict = {}  # key: tilename, value: list(figure shape, figure color, background color)
+
+        f = open("tile.txt", "r")
+        lines = f.readlines()
+        shapeList = lines[0].split()
+        colorList = lines[1].split()
+        bckcolorList = lines[2].split()
+        f.close()
+
+        for shape in shapeList:
+            for color in colorList:
+                for bckcolor in bckcolorList:
+                    name = f"{color}/{shape}/{bckcolor}"
+                    path = f"../image/{color}_{shape}_{bckcolor}.png"
+                    self.tileDict[name] = [color, shape, bckcolor, path]
+
+
+if __name__ == "__main__":
+    t = Tile()
+    print(t.tileDict)
