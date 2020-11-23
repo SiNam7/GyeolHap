18a1e225d5a3a879e1e2703b2ba29445c74c9ff7
SiNam7 <muwon777@naver.com>
2020-11-23 15:43:59 +0900

    Refactor
    디렉토리 명 변경(Logic -> System)
    기타 코드 리팩토링

---
+++ System/gyeolhap.py
@@ -0,0 +1,63 @@
+# from typing import List
+from System.logic import Round
+from System.extra import Player
+# from UI import ui
+
+
+class Gyeolhap:
+
+    rounds = 10
+    # roundFigures: list[Round]
+    timeout = 60  # seconds
+    score = {'G': 1, 'H': 3}
+    penalty = {'G': 1, 'H': 1}
+    gyeolConfirmed = False
+    players = []
+
+    # default game start method: Gyeolhap(player1, player2)
+    def __init__(self, player1: Player, player2: Player):
+
+        # Player setup
+        self.players = [player1, player2]
+        for player in self.players:
+            player.game = self
+        # self.startplayer = self.players[random.randint(0, 2)]
+        self.startplayer = player1
+
+        # Round figure setup
+        self.roundFigures = []
+        for _ in range(self.rounds):
+            self.roundFigures.append(Round())
+
+        # Game start
+        self.currentround = 1
+        while self.currentround <= self.rounds:
+            self.currentfigure = self.roundFigures[self.currentround - 1]
+            # TODO sudo-code / add tiles to figureLayout
+            pass
+
+            while self.gyeolConfirmed is not True:
+                pass
+                # DEBUG
+                self.gyeolConfirmed = True
+
+            self.gyeolConfirmed = False
+            self.currentround += 1
+
+        # TODO / Game over
+        pass
+
+    @classmethod
+    # custom game start method: Gyeolhap.customSetting(player1, player2, rounds, timeouts, score, penalty)
+    def customSetting(cls, player1: Player, player2: Player, rounds: int, timeout: int, score: dict, penalty: dict):
+        cls.rounds = rounds
+        cls.timeout = timeout
+        cls.score = score
+        cls.penalty = penalty
+
+        return cls(player1, player2)
+
+
+if __name__ == "__main__":
+    game = Gyeolhap(Player('A'), Player('B'))
+    print("Success")
---
+++ System/logic.py
@@ -0,0 +1,84 @@
+import itertools
+import random
+
+from GUI.UI import Tile
+from System.extra import Player
+
+# [(0, 'Square/Blue/White'), (1, ...]
+allTileDictionary = [(i, k) for i, k in enumerate(Tile().tileDict.keys())]
+
+
+class Round:
+    # tileDict = {}
+    # answerSet: list = []
+    # answerLog: list = []
+
+    def __init__(self):
+        self.tileDict = {}  # {0: 'Triangle/Blue/Gray' (random), 1: ... }
+        self.answerSet: list = []
+        self.answerLog: list = []
+
+        tileNo = []
+        while len(tileNo) < 9:
+            rand = random.randint(0, 26)
+            if rand not in tileNo:
+                tileNo.append(rand)
+
+        for i, e in enumerate(tileNo):
+            self.tileDict[i] = allTileDictionary[e][1]
+
+        # TODO / pre-calculate all possible answer set, and save it to answerSet
+        self.calculateHap()
+
+    def calculateHap(self):
+        possibleSets = set(itertools.combinations(range(9), 3))  # 숫자 3개
+        for entry in possibleSets:
+            notAnswer = False
+            for i in range(0, 3):  # 0: shape, 1: color, 2: background
+                e = set()
+                for number in entry:
+                    tileinfo = self.tileDict[number].split('/')
+                    e.add(tileinfo[i])
+
+                if len(entry) == 3:
+                    pass
+                else:
+                    notAnswer = True
+                    break
+            if notAnswer:
+                break
+
+            self.answerSet.append(set(entry))
+
+
+def checkHap(player: Player, submitAnswer: set):
+    roundfigure = player.game.currentfigure
+    if submitAnswer in roundfigure.answerSet:
+        roundfigure.answerLog.append(submitAnswer)
+        player.score += player.game.score['H']
+        # TODO / add Changing turn functions
+    else:
+        player.score -= player.game.penalty['H']
+
+
+def checkGyeol(player: Player):
+    roundfigure = player.game.currentfigure
+    if len(roundfigure.answerLog) == len(roundfigure.answerSet):
+        player.score += player.game.score['G']
+        player.game.gyeolConfirmed = True
+        # TODO / add Changing turn functions
+    else:
+        player.score -= player.game.penalty['G']
+
+
+def botSolveAlgorithm(bot: Player, r: Round):
+    answerSet = set(r.answerSet)
+    answerLog = set(r.answerLog)
+
+    possibleAnswer = answerSet - answerLog
+    if len(possibleAnswer) == 0:
+        bot.submitAnswer("G")
+    else:
+        bot.submitAnswer("H", random.sample(possibleAnswer, 1))
+
+
